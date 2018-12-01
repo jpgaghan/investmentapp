00065af26d26ccadb12e5fdbb0424208b6a592f1
rgfathrg <rgfath@gmail.com>
2018-12-01 12:05:44 -0500

    boa

--- tbd-invest/src/App.js
+++ tbd-invest/src/App.js
@@ -10,14 +10,7 @@ class App extends Component {
 
       <div>
         <NavBar />
-<<<<<<< HEAD
-        <div>
-          <Login />
-        </div>
-        
-=======
       
->>>>>>> 50330602f82f88bee9c84a8ab3bc491c6b36a43f
         <div>
           <Footer />
         </div>
