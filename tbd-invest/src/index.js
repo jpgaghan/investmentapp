f84c71d3d4fe48d622b89e878cb7626cf9ddb66b
jcress <cresswellj@outlook.com>
2018-12-01 11:12:01 -0500

    pull

--- tbd-invest/src/index.js
+++ tbd-invest/src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-
-=======
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
->>>>>>> 6bdcff5f22f55cf0b2b33ed61394286e7173ac25
