d72491196f8a81d347d1f4fa21878e0d1f7ad0af
Jonathan Gaghan <jpgaghan@gmail.com>
2018-12-01 13:13:17 -0500

    conditional rendering

--- tbd-invest/src/App.js
+++ tbd-invest/src/App.js
@@ -3,18 +3,44 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
-class App extends Component {
-  render() {
-    return (
 
-      <div>
-        <NavBar />
-      
+class App extends Component {
+  state = {
+    page: "landing"
+  }
+  render() {
+  switch (page) {
+    case page === "landing":
+     
+      return (
+  
         <div>
-          <Footer />
+          <NavBar />
+        
+          <div>
+            <Footer />
+          </div>
         </div>
-      </div>
-    );
+      );
+      break;
+    case page === "main":
+      return (
+    
+        <div>
+          <NavBar />
+        
+          <div>
+            <Footer />
+          </div>
+        </div>
+      );
+      break;
+    default:
+        
+      break;
+  }
+  
+    
   }
 }
 
