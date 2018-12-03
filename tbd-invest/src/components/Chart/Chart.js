fd034738170daca85b1bd26261579b901e699197
Jonathan Gaghan <jpgaghan@gmail.com>
2018-12-03 18:39:17 -0500

    pull

--- tbd-invest/src/components/Chart/Chart.js
+++ tbd-invest/src/components/Chart/Chart.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import SearchForm from "../SearchForm/SearchForm";
+ mport SearchForm from "../SearchForm/SearchForm";
 
 class Chart extends Component {
     state = {
