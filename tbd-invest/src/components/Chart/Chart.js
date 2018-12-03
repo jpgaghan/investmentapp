3792e3d9c2d405d0c6f859b85318feca94004323
rgfathrg <rgfath@gmail.com>
2018-12-03 18:26:13 -0500

    pfff

--- tbd-invest/src/components/Chart/Chart.js
+++ tbd-invest/src/components/Chart/Chart.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import SearchForm from "../Search/SearchForm";
+// import { CanvasJS } from "canvasjs";
+// var CanvasJSReact = require('./canvasjs.react');
+// var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 
 class Charts extends Component {
@@ -9,6 +14,45 @@ class Charts extends Component {
         chartRange: "1d",
         results: [],
     };
+
+    // componentDidMount() {
+    //     var chart = new CanvasJS.Chart("chartContainerD", {
+    //         animationEnabled: true,
+        
+    //         title: {
+    //             text: this.state.ticker + " Daily Chart"
+    //         },
+    //         axisX: {
+    //             title: "Time",
+    //             interval: 15,
+    //             valueFormatString: "hh:mm TT",
+    //             intervalType: "minute"
+    //         },
+    //         axisY: {
+    //             includeZero: false,
+    //             title: "Price Per Share"
+    //         },
+    //         data: [{
+    //             type: "area",
+    //             dataPoints: addDataD()
+    //         }]
+    //     });
+        
+    //     chart.render();
+
+    //     function addDataD() {
+    //         var dataPoints = [];
+    //         var quotes = this.state.results;
+    //         quotes.forEach(function (element) {
+    //             var dateM = element.minute.split(":");
+    //             var hour = dateM[0];
+    //             var minute = dateM[1];
+    //             dataPoints.push({ x: new Date(2018, 2, 9, hour, minute), y: parseFloat(element.close) 
+    //             });
+    //         });
+    //         return(dataPoints);
+    //     }
+    // }
     
     searchHolding = (symbol, range) => {
         API.chart(symbol, range)
@@ -34,10 +78,14 @@ class Charts extends Component {
 
     render() {
         return (
+            <div>
             <SearchForm
             handleFormSubmit={this.handleFormSubmit}
             handleInputChange={this.handleInputChange}
             />
+            <div id="chartContainerD">
+            </div>
+            </div>
         );
     }
 }
