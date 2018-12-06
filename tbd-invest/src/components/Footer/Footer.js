9f2c09bca14e4eae93ee336ea30335f45b74891d
jcress <cresswellj@outlook.com>
2018-12-05 19:21:40 -0500

    puuuuulllllll

--- tbd-invest/src/components/Footer/Footer.js
+++ tbd-invest/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 
 export const Footer = () => (
 <footer className="footer">
-  <div className="container">
+  <div className="container footerCon">
     <span>Project Presented in December, 2018. UNC Charlotte Full Stack Web Developer Program.<br />Active Group Members: Lauren Lapoint, Grant Fath, James Cresswell, Daniel Elston, and Jonathan Gaghan</span>
   </div>
 </footer>
