1724a4e131274059ff43ecc6973e46f55b01665a
Jonathan Gaghan <jpgaghan@gmail.com>
2018-12-14 15:31:32 -0500

    package fix

---
+++ app/build/precache-manifest.63c130001863476902534e6c50183b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fa2c12855ceafea1279734657c33988",
+    "url": "/static/media/slate.7fa2c128.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0363510148cb3840232",
+    "url": "/static/js/main.b0363510.chunk.js"
+  },
+  {
+    "revision": "8d2a2ca4772a0a724534",
+    "url": "/static/js/1.8d2a2ca4.chunk.js"
+  },
+  {
+    "revision": "b0363510148cb3840232",
+    "url": "/static/css/main.d0d83c82.chunk.css"
+  },
+  {
+    "revision": "f0367264d956fd2f9a023bf82c8b9264",
+    "url": "/index.html"
+  }
+];
