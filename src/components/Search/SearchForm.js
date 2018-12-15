778f8a546ad4c12bcf28e6d4e9328dd8683dac59
jcress <cresswellj@outlook.com>
2018-12-14 22:59:27 -0500

    pull

--- src/components/Search/SearchForm.js
+++ src/components/Search/SearchForm.js
@@ -6,16 +6,16 @@ import FinancialData from "../FinancialData/financialdata"
 // import API from "../../utils/API"
 class SearchForm extends Component {
     constructor(props, context) {
-    super(props, context);
-    this.state = {
-        ticker: "",
-        submitted: false,
-        userid: this.props.uid,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-    
+        super(props, context);
+        this.state = {
+            ticker: "",
+            submitted: false,
+            userid: this.props.uid,
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
 
     handleInputChange = event => {
 
@@ -26,74 +26,76 @@ class SearchForm extends Component {
         this.setState({
             [name]: value
         });
-        
+
         if (this.state.ticker === "") {
-        this.setState({
-            submitted: false
-        })
-      }
+            this.setState({
+                submitted: false
+            })
+        }
     };
 
-    
+
     handleFormSubmit = event => {
         event.preventDefault();
-        this.setState({submitted: true})
+        this.setState({ submitted: true })
     };
 
     render() {
-        return !this.state.submitted ?  (
+        return !this.state.submitted ? (
             <div>
                 <form className="search">
                     <h2 className="searchTitle">Stock Search</h2>
                     <div className="form-group">
                         <label htmlFor="ticker">Enter Ticker:</label>
                         <Form inline>
-                            <input
-                                value={this.state.ticker}
-                                onChange={this.handleInputChange}
-                                name="ticker"
-                                className="form-control"
-                                placeholder="AAPL"
-                                id="ticker"
-                            />
-                            <button className='custom-button'
-                                type="submit"
-                                onClick={this.handleFormSubmit}
+                            <div className="searchGroup">
+                                <input
+                                    value={this.state.ticker}
+                                    onChange={this.handleInputChange}
+                                    name="ticker"
+                                    className="form-control"
+                                    placeholder="AAPL"
+                                    id="ticker"
+                                />
+                                <button className='custom-button'
+                                    type="submit"
+                                    onClick={this.handleFormSubmit}
                                 >Search
-                            </button>
+                                </button>
+                            </div>
                         </Form>
                     </div>
-                   
+
                 </form>
-            
-                   
+
+
             </div>
         ) : (<div>
-                <Form className="search">
-                    <h2>Stock Search</h2>
-                    <div className="stockSearchField">
-                        <div className="form-group">
-                            <label htmlFor="ticker">Stock Symbol:</label>
-                            <input
-                                value={this.state.ticker}
-                                onChange={this.handleInputChange}
-                                name="ticker"
-                                className="form-control"
-                                placeholder="Symbol"
-                                id="ticker"
-                            />
-                        </div>
-                        <button className='custom-button'
-                            type="submit"
-                            onClick={this.handleFormSubmit}
-                            >Search
-                        </button>
+            <Form className="search">
+                <h2>Stock Search</h2>
+                <div className="stockSearchField">
+                    <div className="form-group">
+                        <label htmlFor="ticker">Stock Symbol:</label>
+                        <input
+                            value={this.state.ticker}
+                            onChange={this.handleInputChange}
+                            name="ticker"
+                            className="form-control"
+                            placeholder="Symbol"
+                            id="ticker"
+                        />
                     </div>
-                </Form>
-                <Charts ticker={this.state.ticker} submitted={this.state.submitted}/>
-                <FinancialData ticker={this.state.ticker} submitted={this.state.submitted}
-                userid={this.state.userid}/>
-            </div>)
+                    <button className='custom-button'
+                        type="submit"
+                        onClick={this.handleFormSubmit}
+                    >Search
+                        </button>
+                </div>
+            </Form>
+            <Charts ticker={this.state.ticker} submitted={this.state.submitted} />
+            <FinancialData ticker={this.state.ticker} submitted={this.state.submitted}
+                userid={this.state.userid} />
+        </div>)
     }
 };
 
